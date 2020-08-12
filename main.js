0d5928ad66de1ed39f5c544f0e86504d4679f0f8
Ilia <caristovv@gmail.com>
2020-08-12 14:39:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,24 @@ let app = document.querySelector('.wrapper');
 window.addEventListener('wheel', function(e) {
    activeScreensCount = app.querySelectorAll(".screen").length
    console.log(activeScreensCount);
-   if (e.deltaY > 0) app.scrollLeft += app.scrollWidth/activeScreensCount/10;
-   else app.scrollLeft -= app.scrollWidth/activeScreensCount/10;
-   // app.scrollLeft += e.deltaY > 0? (app.scrollWidth/activeScreensCount/10) : -(app.scrollWidth/activeScreensCount/10)
+
+   // if (e.deltaY > 0) app.scrollLeft += app.scrollWidth/activeScreensCount/10;
+   // else app.scrollLeft -= app.scrollWidth/activeScreensCount/10;
+   let counterOut = 0;
+   let ms = 300;
+   let timeOut = 10;
+   let steps = ms/timeOut;
+   if (e.deltaY > 0) {
+      setTimeout(function stepChange(){
+         app.scrollLeft += app.scrollWidth/activeScreensCount/10/steps;
+         console.log(counterOut++);
+         if (counterOut < steps){setTimeout(stepChange,timeOut)}
+      },steps)
+   }else if (e.deltaY < 0){
+      setTimeout(function stepChange(){
+         app.scrollLeft -= app.scrollWidth/activeScreensCount/10/steps;
+         console.log(counterOut++);
+         if (counterOut < steps){setTimeout(stepChange,timeOut)}
+      },steps)
+   }
 });
