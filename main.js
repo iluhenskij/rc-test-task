decd2e13c2df1e2a09e18a5973268b15775ef7c1
Ilia <caristovv@gmail.com>
2020-08-24 05:44:52 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function goToPage(pageNum){
    pagePosition = pageNum;
    console.log(pageNum);
    styleTag.innerHTML = `.screen{transform: translate(-${pageNum}%, 0%)}`
+   checkSpecialPage()
 }
 
 //SCREEN6
@@ -20,7 +21,7 @@ typesOfRecieps.addEventListener("click",(event)=>{
    item.addEventListener("click",function selectElem(event){
       console.log(this);
       console.dir(event);
-      this.parentElement.lastElementChild.lastElementChild.classList.toggle("opacity1")
+      this.parentElement.children[1].lastElementChild.classList.toggle("opacity1")
       this.lastElementChild.classList.toggle("closed")
    })
 })
@@ -28,25 +29,56 @@ typesOfRecieps.addEventListener("click",(event)=>{
 
 // MENU HEADER
 const logo = document.querySelector(".header__logo")
+const menuButton = document.querySelector(".header")
+const whiteLogo = document.querySelector(".white_logo_block")
+
 logo.addEventListener("click",()=>{goToPage(1)})
 
 
 const menuBotton = document.querySelector(".menu_button")
-function menuBottonOpen(event){
+function menuOpen(event){
+   menuBotton.removeEventListener("click",menuOpen)
+   window.removeEventListener('wheel', scrollOnTranslate)
+   window.removeEventListener('wheel', mobileScrollOnTranslate)
    console.log("opening...");
    menuBlock.style.zIndex = 1300
    setTimeout(()=>{
       menuBlock.classList.add("opacity1")
    },50)
-
+   fadeIt(logo)
+   fadeIt(pageName)
+   menuBotton.classList.add("open")
+   menuBotton.classList.add("white")
+   setTimeout(()=> {
+      logo.classList.add("fadeZindex");
+      document.addEventListener('click',menuCloseWithButton)
+   },500)
+   
 }
 function menuClose(){
    menuBlock.classList.remove("opacity1")
    setTimeout(()=>{
       menuBlock.style.zIndex = -1300
    },500)
+   window.addEventListener('wheel', scrollOnTranslate)
+   window.addEventListener('wheel', mobileScrollOnTranslate)
 }
-menuBotton.addEventListener("click",menuBottonOpen)
+menuBotton.addEventListener("click",menuOpen)
+
+function menuCloseWithButton(){
+   document.removeEventListener('click',menuCloseWithButton)
+   menuBlock.classList.remove("opacity1")
+   setTimeout(()=>{
+      menuBlock.style.zIndex = -1300
+   },500)
+   menuBotton.classList.remove("open")
+   if (pagePosition!==7){menuBotton.classList.remove("white")}
+   checkSpecialPage()
+   menuBotton.addEventListener("click",menuOpen)
+   window.addEventListener('wheel', scrollOnTranslate)
+   window.addEventListener('wheel', mobileScrollOnTranslate)
+}
+
 //MENU MAIN
 const menuBlock = document.querySelector(".screen_menu")
 menuBlock.addEventListener("click",(event)=>{
@@ -55,30 +87,223 @@ menuBlock.addEventListener("click",(event)=>{
    goToPage(event.target.getAttribute("go-from-menu"));
 })
 
+//MENU LEFT DISCRIPTION OF PAGE
+const pageName = document.querySelector(".page_left_disription__text")
 
+//MENU FOOTER
+const footer = document.querySelector(".footer")
+const footerSites = document.querySelector(".footer__social_sites")
+
+const buttonNextScreen = document.querySelector(".footer__next_scr")
+buttonNextScreen.addEventListener("click",()=>{
+   let fakeEvent = {deltaX:100,deltaY:100}
+   scrollOnTranslate(fakeEvent)
+})
+
+
+//SPECIAL RUELS FOR MENU
+let specialRules = [
+   function(){
+      changeHeaderColor("black")
+      console.log("im first spec func 0")//SCREEN 1
+      changeMenuStateFade([true,true,true,true])
+      fadeIt(pageName)
+   },
+   function(){
+      changeHeaderColor("black")
+      console.log("im first spec func 1")//SCREEN 2
+      changeMenuStateAppear([true,true,true,true,])
+      fadeIt(pageName)
+   },
+   function(){
+      changeHeaderColor("black")
+      console.log("im first spec func 2")//SCREEN 3
+      changeMenuStateAppear([true,true,true,true,])
+      fadeIt(pageName)
+      setTimeout(()=>{
+         pageName.innerText = "О редисе"
+         appearIt(pageName)
+
+      },500)
+
+   },
+   function(){
+      changeHeaderColor("black")
+      console.log("im first spec func 3")//SCREEN 4
+      changeMenuStateAppear([true,true,true,true,])
+      fadeIt(pageName)
+      setTimeout(()=>{
+         pageName.innerText = "полезные свойства"
+         appearIt(pageName)
+
+      },500)
+
+   },
+   function(){
+      changeHeaderColor("black")
+      console.log("im first spec func 4")//SCREEN 5
+      changeMenuStateAppear([true,true,true,true,])
+      fadeIt(pageName)
+      setTimeout(()=>{
+         pageName.innerText = "стоимость"
+         appearIt(pageName)
+
+      },500)
+
+   },
+   function(){
+      changeHeaderColor("black")
+      console.log("im first spec func 5")//SCREEN 6
+      changeMenuStateAppear([true,true,true,true,])
+      fadeIt(pageName)
+      setTimeout(()=>{
+         pageName.innerText = "рецепты"
+         appearIt(pageName)
+
+      },500)
+
+   },
+   function(){
+      changeHeaderColor("white")
+      console.log("im first spec func 6")//SCREEN 7
+      changeMenuStateAppear([true,true,true,true,])
+      fadeIt(pageName)
+      setTimeout(()=>{
+         pageName.innerText = "все блюда"
+         appearIt(pageName)
+
+      },500)
+
+   },
+   function(){
+      changeHeaderColor("black")
+      console.log("im first spec func 7")//SCREEN 8
+      changeMenuStateAppear([true,true,true,true,])
+      fadeIt(pageName)
+      setTimeout(()=>{
+         pageName.innerText = "окрошка на кефире"
+         appearIt(pageName)
+
+      },500)
+
+   },
+   function(){
+      changeHeaderColor("black")
+      console.log("im first spec func 8")//SCREEN 9
+      changeMenuStateAppear([true,true,true,true,])
+      fadeIt(pageName)
+      setTimeout(()=>{
+         pageName.innerText = "окрошка на кефире"
+         appearIt(pageName)
+
+      },500)
+
+   },
+   function(){
+      changeHeaderColor("black")
+      console.log("im first spec func 9")//SCREEN 10
+      changeMenuStateFade([false,false,true,false])
+      changeMenuStateAppear([true,true,false,true])
+      fadeIt(pageName)
+      setTimeout(()=>{
+         pageName.innerText = "старт продаж"
+         appearIt(pageName)
+
+      },500)
+
+   },
+]
+
+
+
+//MENU CHANGING
+function changeMenuStateFade(arr){
+   if (arr[0]) {
+      logo.classList.add("fade");
+      logo.classList.add("fadeZindex")
+   }
+   if (arr[1]) {
+      menuButton.classList.add("fade");
+      menuButton.classList.add("fadeZindex")
+   }
+   if (arr[2]) {
+      footer.classList.add("fade");
+      footer.classList.add("fadeZindex")
+   }
+   if (arr[3]) {
+      footerSites.classList.add("fade");
+      footerSites.classList.add("fadeZindex")
+   }
+}
+function changeMenuStateAppear(arr){
+   if (arr[0]) {
+      logo.classList.remove("fadeZindex");
+      setTimeout(() => {
+         logo.classList.remove("fade")
+      }, 100)
+   }
+   if (arr[1]) {
+      menuButton.classList.remove("fadeZindex");
+      setTimeout(() => {
+         menuButton.classList.remove("fade")
+      }, 100)
+   }
+   if (arr[2]) {
+      footer.classList.remove("fadeZindex");
+      setTimeout(() => {
+         footer.classList.remove("fade")
+      }, 100)
+   }
+   if (arr[3]) {
+      footerSites.classList.remove("fadeZindex");
+      setTimeout(() => {
+         footerSites.classList.remove("fade")
+      }, 100)
+   }
+}
+function changeHeaderColor(color){
+   if (color=="white"){
+      whiteLogo.classList.add("active")
+      menuBotton.classList.add("white")
+
+   }
+   if (color=="black") {
+      whiteLogo.classList.remove("active")
+      menuBotton.classList.remove("white")
+   }
+}
 
 //SCROLL
+function checkSpecialPage(pageNum){
+   let pagePos = pagePosition/100;
+   // console.log(pagePos)
+   specialRules[pagePos]()
+}
+
 let touchStartInfo = null;
 let touchEndInfo = null;
 let pagePosition = 0;
-function scroolOnTranslate(event){
+
+function scrollOnTranslate(event){
    // console.log("working" ,event);
    setTimeout(()=> {
-      window.addEventListener('wheel', scroolOnTranslate)
+      window.addEventListener('wheel', scrollOnTranslate)
    },200)
-   window.removeEventListener("wheel",scroolOnTranslate)
+   window.removeEventListener("wheel",scrollOnTranslate)
    if (event.deltaY>0||event.deltaX>0){
       pagePosition>=900?{}:pagePosition += 100;
       styleTag.innerHTML = `.screen{transform: translate(-${pagePosition}%, 0%)}`
-   }else{
+   }
+   else{
       pagePosition<=0?{}:pagePosition -= 100;
       styleTag.innerHTML = `.screen{transform: translate(-${pagePosition}%, 0%)}`
    }
+   checkSpecialPage()
 }
-window.addEventListener('wheel', scroolOnTranslate);
+window.addEventListener('wheel', scrollOnTranslate);
 
 //SCROLL MOBILE
-function mobileScroolOnTranslate(event){
+function mobileScrollOnTranslate(event){
    // console.log("working mobile" ,event);
    if (event.deltaX>40){
       pagePosition>=900?{}:pagePosition += 100;
@@ -88,11 +313,14 @@ function mobileScroolOnTranslate(event){
       pagePosition<=0?{}:pagePosition -= 100;
       styleTag.innerHTML = `.screen{transform: translate(-${pagePosition}%, 0%)}`
    }
+   checkSpecialPage()
+
 }
 window.addEventListener("touchstart",(event)=> {
    touchStartInfo = event.changedTouches[0];
    touchEndInfo;
    // console.log(event);
+
 })
 window.addEventListener("touchend",(event)=> {
    // console.log(event);
@@ -100,183 +328,17 @@ window.addEventListener("touchend",(event)=> {
    // console.log([touchStartInfo, touchEndInfo])
    fakeDeltaX = touchStartInfo.clientX - touchEndInfo.clientX
    let fakeEvent = {deltaX:fakeDeltaX}
-   mobileScroolOnTranslate(fakeEvent);
+   mobileScrollOnTranslate(fakeEvent);
    touchStartInfo = null;
    touchEndInfo = null;
 })
 
-
-
-
-
-
-
-/*function handleGoFromMenuTo(event){
-   menuScreen.classList.remove("opacity1");
-   menuScreen.style.zIndex = -1000;
-   window.addEventListener("wheel",customScroll)
-   let whereGo = document.querySelector(event.target.getAttribute("go-from-menu"));
-   console.log(whereGo);
-   whereGo.scrollIntoView({block: "center", behavior: "smooth"});
-   closeMenu()
+//global functions
+function fadeIt(elem){
+   elem.classList.add("fade")
 }
-function handleOpenMenu(event){
-   if(!event.target.classList.contains("menu")) return
-   /!*going to screen of clicked menu*!/
-   let screenTarget = event.target.getAttribute("located-on-scr");
-   let screenNum = screenTarget.slice(4)
-   console.log(screenTarget, screenNum);
-   document.querySelector(screenTarget).scrollIntoView({block: "center", behavior: "smooth"});
-   let fullMenu = document.querySelector(".screen_menu");
-   console.log(fullMenu);
-   fullMenu.style.gridColumn = `${screenNum}/${+screenNum+1}`
-   fullMenu.style.zIndex = 10000;
-   app.style.overflowX = "hidden"
-   setTimeout(()=>fullMenu.classList.add("opacity1"),200)
-   setTimeout(()=>app.style.overflowX = "hidden",900)
-
-   window.removeEventListener("wheel",customScroll)
-}
-function closeMenu(){
-   menuScreen.classList.remove("oppac1");
-   menuScreen.style.zIndex = -1000;
-   app.style.overflowX = "initial"
-
-   window.addEventListener("wheel",customScroll)
-}*/
-// function handleGoToNextScreen(event) {
-//    if (!event.target.getAttribute("next-scr")) return;
-//    let whereGo = document.querySelector(event.target.getAttribute("next-scr"));
-//    whereGo.scrollIntoView(/*{block: "center", behavior: "smooth"}*/);
-//    console.log(whereGo);
-// }
-/*document.addEventListener("click",handleGoToNextScreen)
-document.addEventListener("click",handleOpenMenu)
-document.addEventListener("click",(event)=>{
-   if (!event.target.classList.contains("logo")) return;
-   document.querySelector("#scr1").scrollIntoView({block: "center", behavior: "smooth"})
-})
-menuScreenCloseButton.addEventListener("click",closeMenu)
-menuButtonsBlock.addEventListener("click",handleGoFromMenuTo)*/
-
-/*let customScrollState = false;
-function customScroll(event){
-   if (customScrollState) return
-   customScrollState = true
-   let width = app.offsetWidth;
-
-   let animationTime = 500;
-   let animationStepsCount = 100;
-   let animationStepTime = animationTime/animationStepsCount;
-
-   let stepWidth = width/animationStepsCount
-   console.log(event.deltaY);
-   let animationEnd = width % animationStepsCount
-   animationEnd = 25
-   console.log(animationEnd);
-   if(event.deltaY>0){
-      let scrollWidthMade = 0;
-      let idT = setInterval(()=>{
-         app.scrollLeft += stepWidth;
-         scrollWidthMade++;
-         console.log(stepWidth);
-         if(scrollWidthMade==animationStepsCount){
-            customScrollState = false
-            clearInterval(idT)
-            let animEndChecker = 0;
-            let animEndId = setInterval(()=>{
-               app.scrollLeft++;
-               animEndChecker++;
-               if(animEndChecker==animationEnd)clearInterval(animEndId)
-            },10)
-            // justifyScreen()
-         }
-      },animationStepTime)
-
-   }
-   if(event.deltaY<0){
-      let scrollWidthMade = 0;
-      let idT = setInterval(()=>{
-         app.scrollLeft -= stepWidth;
-         scrollWidthMade++;
-         console.log(stepWidth);
-         if(scrollWidthMade==animationStepsCount){
-            customScrollState = false
-            clearInterval(idT)
-            // justifyScreen()
-         }
-      },animationStepTime)
-   }
-   console.log(stepWidth*animationStepsCount);
-   /!*let counter = 0;
-   activeScreensCount = app.querySelectorAll(".screen").length
-   console.log(activeScreensCount);
-   let step = app.scrollWidth/activeScreensCount/10
-   if (event.deltaY > 0){
-      let timerId = setInterval(()=>{
-         app.scrollLeft += app.scrollWidth/activeScreensCount/10/10 ;
-         counter++
-         counter==10?clearTimeout(timerId):{};
-      },100/10)
-   }
-   else{
-      let timerId = setInterval(()=>{
-         app.scrollLeft -= app.scrollWidth/activeScreensCount/10/10 ;
-         counter++
-         counter==10?clearTimeout(timerId):{};
-      },100/10)
-   }*!/
-}*/
-// let scrollId = window.addEventListener('wheel', customScroll);
-
-/*
-function sideScroll(element,direction,speed,distance,step){
-   scrollAmount = 0;
-   var slideTimer = setInterval(function(){
-      if(direction == 'left'){
-         element.scrollLeft -= step;
-      } else {
-         element.scrollLeft += step;
-      }
-      scrollAmount += step;
-      if(scrollAmount >= distance){
-         window.clearInterval(slideTimer);
-      }
-   }, speed);
-}
-*/
-
-/*let app = document.querySelector('.wrapper');
-let scrolMade = 0
-function scrollHandler(e) {
-   console.log("scroll started");
-   activeScreensCount = app.querySelectorAll(".screen").length
-   console.log(activeScreensCount);
-   window.removeEventListener("wheel",scrollHandler)
-   // if (e.deltaY > 0) app.scrollLeft += app.scrollWidth/activeScreensCount/10;
-   // else app.scrollLeft -= app.scrollWidth/activeScreensCount/10;
-   let counterOut = 0;
-   let ms = 300;
-   let timeOut = 20;
-   let steps = ms/timeOut;
-   let move = app.scrollWidth/activeScreensCount/10/steps
-   if (e.deltaY > 0) {
-      setTimeout(function stepChange(){
-         app.scrollLeft += move;
-         counterOut++;
-         if (counterOut <= steps){setTimeout(stepChange,timeOut)}
-      },steps)
-   }else if (e.deltaY < 0){
-      setTimeout(function stepChange(){
-         app.scrollLeft -= move;
-         counterOut++;
-         if (counterOut <= steps){setTimeout(stepChange,timeOut)}
-      },steps)
-
-   }
-   window.addEventListener('wheel', scrollHandler);
-   console.log("scroll ended");
-
+function appearIt(elem){
+   elem.classList.remove("fade")
 }
 
-window.addEventListener('wheel', scrollHandler);*/
+checkSpecialPage()
